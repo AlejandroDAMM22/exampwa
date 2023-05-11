6b93ae0ca6de4d9e8d759039718f164430a66826
Alejandro Morales <116979446+AlejandroDAMM22@users.noreply.github.com>
2023-05-11 08:49:11 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ self.addEventListener('install', event=>{
               .then(cache=>{
                     return cache.addAll([
                         '/',
-                        'exampwa/index.html',
+                        'index.html',
                         'img/error-404.png',
                         'exampwa/img/noImage.png',
                         'exampwa/img/agua.png',
